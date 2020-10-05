da7cdbc0f2ef0332471117e153591b2f6ca82575
Chris Golden <chrgolden@gmail.com>
2020-10-05 13:01:54 -0700

    organized variables

--- src/db.js
+++ src/db.js
@@ -2,8 +2,10 @@ let db = {
   name: "chris",
   creator: "chris",
   SP: {
-    handleCOUNT: ({ value, database}) => {
-      const valueOccurrences = Object.keys(db).filter((key) => database[key] === value);
+    handleCOUNT: ({ database, value }) => {
+      const valueOccurrences = Object.keys(db).filter(
+        (key) => database[key] === value
+      );
       console.log(valueOccurrences);
       return `${valueOccurrences.length} ${
         valueOccurrences.length > 1 ? "keys" : "key"
@@ -11,34 +13,34 @@ let db = {
         valueOccurrences.length && valueOccurrences.map((key) => `${key}\n`)
       }`;
     },
-    handleDELETE: ({ key, database}) => {
+    handleDELETE: ({ database, key }) => {
       const keyExists = database[key];
       keyExists && delete database[key];
       return keyExists
-      ? `"${key}" deleted successfully.`
-      : `"${key}" does not exist in database.`;
+        ? `"${key}" deleted successfully.`
+        : `"${key}" does not exist in database.`;
     },
-    handleGET: ({ key, database}) => {
+    handleGET: ({ database, key }) => {
       return (
         (database[key] && `${key}: ${database[key]}`) ||
         `"${key}" does not exist in database.`
-        );
-      },
-      handleSET: ({ key, value, database}) => {
-        const keyExists = database[key];
-        !keyExists && (database[key] = value);
-        return keyExists
+      );
+    },
+    handleSET: ({ database, key, value }) => {
+      const keyExists = database[key];
+      !keyExists && (database[key] = value);
+      return keyExists
         ? `Key "${key}" already exists in db. Use UPDATE to change this key's value.`
         : `Successfully added "${key}" = "${value}".`;
-      },
-      handleUPDATE: ({ key, value, database}) => {
-        const keyExists = database[key];
-        keyExists && (database[key] = value);
-        return keyExists
+    },
+    handleUPDATE: ({ database, key, value }) => {
+      const keyExists = database[key];
+      keyExists && (database[key] = value);
+      return keyExists
         ? `Successfully updated: "${key}" = "${value}".`
         : `Key "${key}" doesn't exist in db.\nUse SET to add this key and value.`;
-      },
-    }
+    },
+  },
 };
 
 module.exports = db;
